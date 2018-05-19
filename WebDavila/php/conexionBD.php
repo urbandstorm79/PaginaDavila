06cf23527a2b3e7346fec5a2b84c7c65dc066467
Urbano Gonzalez <urbano.gonzalez@alumnos.udg.mx>
2018-05-19 12:31:01 -0500

    WebDavila

--- WebDavila/php/conexionBD.php
+++ WebDavila/php/conexionBD.php
@@ -4,11 +4,11 @@ $username = "root";
 $password = "";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, "bdproductos");
+$conn = new mysqli($servername, $username, $password, "BDPedidos");
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Conexion exitosa";
 ?>
